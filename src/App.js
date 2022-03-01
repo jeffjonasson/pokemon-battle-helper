f15923425c9d99f3b1e74ab25977eb890dfed52f
Jeff Jonasson <jeff.jonasson@gmail.com>
2022-03-01 22:15:19 +0100

    Create react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Pokémon Battle Helper</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
