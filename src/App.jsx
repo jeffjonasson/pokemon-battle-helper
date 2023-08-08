cfd5fefa540ff3af24c5df3dc99060ff45a243ff
Jeff Jonasson <jeff.jonasson@gmail.com>
2023-08-08 13:18:09 +0200

    Add buttons for next and previous pokemon, update styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
-import React, { useEffect, useState } from "react";
-import { Autocomplete, Paper, TextField } from "@mui/material";
-import Pokedex from "pokedex-promise-v2";
-import "./App.css";
+import React, { useEffect, useState } from 'react';
+import { Autocomplete, Paper, TextField } from '@mui/material';
+import Pokedex from 'pokedex-promise-v2';
+import './App.css';
 import {
   pokemonNameFormatter,
   pokemonTypesExtractor,
   defenseCalculator,
   pokemonTypeFormatter,
-} from "./helpers";
-import { DefenseStats } from "./DefenseStats";
+} from './helpers';
+import { DefenseStats } from './DefenseStats';
+import { ReactComponent as LeftArrow } from './assets/left-arrow.svg';
 
 const App = () => {
+  const initalState = {
+    current: null,
+    next: null,
+    previous: null,
+  };
   const [pokemonList, setPokemonList] = useState([]);
-  const [pokemon, setPokemon] = useState(null);
+  const [pokemon, setPokemon] = useState(initalState);
   const P = new Pokedex();
   const interval = {
     limit: 898,
@@ -28,7 +34,10 @@ const App = () => {
   const fetchPokemonData = (url) => {
     const trimmedUrl = url.substring(19);
     P.getResource(trimmedUrl).then((response) => {
-      setPokemon(response);
+      const index = pokemonList.findIndex((e) => e.url === url);
+      const next = index + 1 > interval.limit ? null : pokemonList[index + 1];
+      const previous = index - 1 < 0 ? null : pokemonList[index - 1];
+      setPokemon({ current: response, next, previous });
     });
   };
 
@@ -36,17 +45,19 @@ const App = () => {
     if (newValue) {
       fetchPokemonData(newValue.url);
       event.target.blur();
-    } else setPokemon(null);
+    } else setPokemon(initalState);
   };
 
+  window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+
   return (
-    <div className="App">
-      <div className="Content">
-        <header className="App-header">
-          <h2>Pokémon Battle Helper</h2>
+    <div className='App'>
+      <div className='Content'>
+        <header className='App-header'>
+          <span>Pokémon Battle Helper</span>
         </header>
-        <div className="InputFieldWrapper">
-          <div className="InputField">
+        <div className='InputFieldWrapper'>
+          <div className='InputField'>
             <Autocomplete
               freeSolo
               onChange={onChange}
@@ -58,17 +69,17 @@ const App = () => {
               renderInput={(params) => (
                 <>
                   <Paper
-                    component="form"
+                    component='form'
                     sx={{
-                      p: "6px",
-                      display: "flex",
-                      alignItems: "center",
+                      p: '6px',
+                      display: 'flex',
+                      alignItems: 'center',
                     }}
                   >
                     <TextField
                       fullWidth
                       {...params}
-                      label="Search for a pokémon"
+                      label='Search for a pokémon'
                     />
                   </Paper>
                 </>
@@ -76,26 +87,56 @@ const App = () => {
             />
           </div>
         </div>
-        {pokemon && (
+        {pokemon.current && (
           <div>
-            <div className="PokemonName">
-              <span>{pokemonNameFormatter(pokemon.name)}</span>
+            <div className='PrevAndNextPokemon'>
+              {pokemon.previous && (
+                <div className='PreviousPokemon'>
+                  <button
+                    className='SelectButton'
+                    onClick={() => fetchPokemonData(pokemon.previous.url)}
+                  >
+                    <span>{pokemonNameFormatter(pokemon.previous.name)}</span>
+                    <div className='LeftArrow'>
+                      <LeftArrow />
+                    </div>
+                  </button>
+                </div>
+              )}
+              {pokemon.next && (
+                <div className='NextPokemon'>
+                  <button
+                    className='SelectButton'
+                    onClick={() => fetchPokemonData(pokemon.next.url)}
+                  >
+                    <span>{pokemonNameFormatter(pokemon.next.name)}</span>
+                    <div className='RightArrow'>
+                      <LeftArrow />
+                    </div>
+                  </button>
+                </div>
+              )}
             </div>
-            <img alt="Pokemon" src={pokemon.sprites.front_default} />
-            <div className="PokemonType">
+            <div className='PokemonName'>
+              <span>{pokemonNameFormatter(pokemon.current.name)}</span>
+            </div>
+            <img alt='Pokemon' src={pokemon.current.sprites.front_default} />
+            <div className='PokemonType'>
               <span>
-                {pokemonTypeFormatter(pokemonTypesExtractor(pokemon))}
+                {pokemonTypeFormatter(pokemonTypesExtractor(pokemon.current))}
               </span>
             </div>
             <div>
               <DefenseStats
-                defenseStats={defenseCalculator(pokemonTypesExtractor(pokemon))}
+                defenseStats={defenseCalculator(
+                  pokemonTypesExtractor(pokemon.current)
+                )}
               />
             </div>
           </div>
         )}
       </div>
-      <div className="Footer">
+      <div className='Footer'>
         <div>Created by Jeff Jonasson. © 2022</div>
         <div>
           Pokémon © 2002–2022 Pokémon. © 1995–2022 Nintendo/Creatures Inc./GAME
