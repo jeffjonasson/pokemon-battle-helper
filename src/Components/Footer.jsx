0021904960af360a21c3e302afe7ea69c0e126d3
Jeff Jonasson <jeff.jonasson@gmail.com>
2024-02-09 16:20:43 +0100

    Update year in footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,8 @@
 const Footer = () => (
   <div className='Footer'>
-    <div>Created by Jeff Jonasson. © 2022</div>
+    <div>Created by Jeff Jonasson. © 2024</div>
     <div>
-      Pokémon © 2002–2022 Pokémon. © 1995–2022 Nintendo/Creatures Inc./GAME
+      Pokémon © 2002–2024 Pokémon. © 1995–2024 Nintendo/Creatures Inc./GAME
       FREAK inc. ™, ® and Pokémon character names are trademarks of Nintendo. No
       copyright or trademark infringement is intended in using Pokémon content
       on this page.
