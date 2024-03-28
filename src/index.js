3043eb446b17ff388aceaa4a373db0e2dc75c72b
Jeff Jonasson <jeff.jonasson@gmail.com>
2024-03-28 10:14:05 +0100

    Add speed insights

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 ReactDOM.render(
   <React.StrictMode>
+    <SpeedInsights />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
